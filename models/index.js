a3e0627559337914565bd8f613afe8e3ab94e07b
Ana Moros <ana.morosmarmol@sky.uk>
2022-11-04 09:40:09 +0000

    added associations between the models

--- models/index.js
+++ models/index.js
@@ -2,4 +2,15 @@ const Cheese = require("./Cheese.model");
 const Board = require("./Board.model");
 const User = require("./User.model");
 
+// Users < - > Boards: one-to-many
+// multiple Boards can be added to a User
+User.hasMany(Board);
+Board.belongsTo(User);
+
+// Boards < - > Cheeses: many-to-many
+// Board can have many Cheeses
+// Cheese can be on many Boards
+Board.belongsToMany(Cheese, { through: "Cheeseboard" });
+Cheese.belongsToMany(Board, { through: "Cheeseboard" });
+
 module.exports = { Cheese, Board, User };
